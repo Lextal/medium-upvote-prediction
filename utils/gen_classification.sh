1ba302d4f67e6058322a87137417302a5759739a
Roman Trusov <gimmepython@gmail.com>
2017-03-17 12:00:45 +0300

    single script for generating classification dataset + cleanup

---
+++ utils/gen_classification.sh
@@ -0,0 +1,10 @@
+src_dir=$1
+
+echo "Performing train/test split"
+python train_test_split.py ${src_dir}/medium.json ${src_dir}/train.json ${src_dir}/test.json ${src_dir}/index
+
+echo "Generating training dataset for fasttext"
+python data_loader.py ${src_dir}/train.json > ${src_dir}/train.txt
+
+echo "Generating validation dataset for fasttext"
+python data_loader.py ${src_dir}/train.json > ${src_dir}/test.txt
